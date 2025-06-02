3114c437c4b095dc42690775696f2ac017fb6ac4
Thomas Darimont <thomas.darimont@googlemail.com>
2025-06-02 22:04:28 +0200

    Fix blackscreen at start bug on MacOS

--- src/main/java/com/stepstone/jc/demo/GameWindow.java
+++ src/main/java/com/stepstone/jc/demo/GameWindow.java
@@ -2,13 +2,14 @@ package com.stepstone.jc.demo;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.function.Consumer;
 
 import static com.stepstone.jc.demo.Doom.doomScreenHeight;
 import static com.stepstone.jc.demo.Doom.doomScreenWidth;
 
 public class GameWindow extends JFrame {
-    private final JPanel innerPanel = new JPanel();
+    private final GamePanel innerPanel = new GamePanel();
     private final DoomKeyListener keyListener = new DoomKeyListener();
 
     public GameWindow() throws HeadlessException {
@@ -24,11 +25,30 @@ public class GameWindow extends JFrame {
         pack();
     }
 
-    void drawImage(Image image) {
-        innerPanel.getGraphics().drawImage(image, 0, 0, innerPanel.getWidth(), innerPanel.getHeight(), null);
+    void drawImage(BufferedImage image) {
+        innerPanel.setImage(image);
+        innerPanel.repaint(); // Trigger a repaint when the image is updated
     }
 
     public void drainKeyEvents(Consumer<int[]> action) {
         keyListener.drainEvents(action);
+        invalidate();
+    }
+
+    // Custom JPanel to handle image rendering
+    private static class GamePanel extends JPanel {
+        private BufferedImage image;
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            if (image != null) {
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+            }
+        }
+
+        public void setImage(BufferedImage image) {
+            this.image = image;
+        }
     }
 }
